b57f0878ec9da63942bd50ca2ea6dbec4aab7286
Christian Charon <christian.charon@mailbox.org>
2026-01-04 20:43:21 +0100

    menu system

--- GameLibrary/Core.cs
+++ GameLibrary/Core.cs
@@ -1,5 +1,7 @@
 using System;
+using GameLibrary.Audio;
 using GameLibrary.Input;
+using GameLibrary.Scenes;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -9,12 +11,18 @@ namespace GameLibrary;
 
 public class Core : Game
 {
-    private static Core s_instance;
+    internal static Core s_instance;
 
     /// <summary>
     /// Gets a reference to the Core instance.
     /// </summary>
     public static Core Instance => s_instance;
+
+    // The scene that is currently active.
+    private static Scene s_activeScene;
+
+    // The next scene to switch to, if there is one.
+    private static Scene s_nextScene;
 
     /// <summary>
     /// Gets the graphics device manager to control the presentation of graphics.
@@ -45,6 +53,11 @@ public class Core : Game
     /// Gets or Sets a value that indicates if the game should exit when the esc key on the keyboard is pressed.
     /// </summary>
     public static bool ExitOnEscape { get; set; }
+    
+    /// <summary>
+    /// Gets a reference to the audio control system.
+    /// </summary>
+    public static AudioController Audio { get; private set; }
     
     /// <summary>
     /// Creates a new Core instance.
@@ -105,6 +118,17 @@ public class Core : Game
         
         // Create a new input manager.
         Input = new InputManager();
+        
+        // Create a new audio controller.
+        Audio = new AudioController();
+    }
+    
+    protected override void UnloadContent()
+    {
+        // Dispose of the audio controller.
+        Audio.Dispose();
+
+        base.UnloadContent();
     }
     
     protected override void Update(GameTime gameTime)
@@ -112,11 +136,74 @@ public class Core : Game
         // Update the input manager.
         Input.Update(gameTime);
 
+        // Update the audio controller.
+        Audio.Update();
+        
         if (ExitOnEscape && Input.Keyboard.IsKeyDown(Keys.Escape))
         {
             Exit();
         }
 
+        // if there is a next scene waiting to be switch to, then transition
+        // to that scene.
+        if (s_nextScene != null)
+        {
+            TransitionScene();
+        }
+
+        // If there is an active scene, update it.
+        if (s_activeScene != null)
+        {
+            s_activeScene.Update(gameTime);
+        }
+
         base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        // If there is an active scene, draw it.
+        if (s_activeScene != null)
+        {
+            s_activeScene.Draw(gameTime);
+        }
+
+        base.Draw(gameTime);
+    }
+
+    public static void ChangeScene(Scene next)
+    {
+        // Only set the next scene value if it is not the same
+        // instance as the currently active scene.
+        if (s_activeScene != next)
+        {
+            s_nextScene = next;
+        }
+    }
+
+    private static void TransitionScene()
+    {
+        // If there is an active scene, dispose of it.
+        if (s_activeScene != null)
+        {
+            s_activeScene.Dispose();
+        }
+
+        // Force the garbage collector to collect to ensure memory is cleared.
+        GC.Collect();
+
+        // Change the currently active scene to the new scene.
+        s_activeScene = s_nextScene;
+
+        // Null out the next scene value so it does not trigger a change over and over.
+        s_nextScene = null;
+
+        // If the active scene now is not null, initialize it.
+        // Remember, just like with Game, the Initialize call also calls the
+        // Scene.LoadContent
+        if (s_activeScene != null)
+        {
+            s_activeScene.Initialize();
+        }
     }
 }
